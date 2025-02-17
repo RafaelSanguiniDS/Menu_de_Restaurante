a3cef7fae3727a73b8ed345acd9ff5ae97734713
Rafael Sanguini de Sousa <rafinha.sanguinii@gmail.com>
2025-02-17 15:56:23 -0300

    implementado cadastros de pratos e sobremesas e consulta de produtos

--- Menu.py
+++ Menu.py
@@ -1,9 +1,60 @@
-class restaurante:
+class menu:
+    def __init__(self):
+        self.bebidas = []
+        self.pratos = []
+        self.drinks = []
+        self.sobremesa = []
+
+
+    def adicionar_bebida(self, bebida):
+        self.bebidas.append(bebida)
+    
+    def adicionar_prato(self, prato):
+        self.pratos.append(prato)
+    
+    def adicionar_sobremesa(self, sobremesa):
+        self.sobremesa.append(sobremesa)
+    
+    def adicionar_drink(self, drink):
+        self.drinks.append(drink)
+
+    def consultar_bebidas(self):
+        for bebida in self.bebidas:
+            bebida.escrever_nome()
+            bebida.escrever_valor()
+            bebida.escrever_descriçao()
+            print('')
+    
+    def consultar_pratos(self):
+        for prato in self.pratos:
+            prato.escrever_nome()
+            prato.escrever_valor()
+            prato.escrever_descriçao()
+            print('')
+
+    def consultar_drinks(self):
+        for drink in self.drinks:
+            drink.escrever_nome()
+            drink.escrever_valor()
+            drink.escrever_descriçao()
+            print('')
+
+    def consultar_sobremesas(self):
+        for sobremesa in self.sobremesa:
+            sobremesa.escrever_nome()
+            sobremesa.escrever_valor()
+            sobremesa.escrever_descriçao()
+            print('')
+
+
+
+
+
+class Bebida:
     def __init__(self):
         self.nome = ''
         self.valor = ''
-        self.descriçao= ''
-
+        self.descriçao = ''
 
     def adicionar_nome(self, nome):
         self.nome = nome
@@ -23,16 +74,74 @@ class restaurante:
     def escrever_descriçao(self):
         print(f'descriçao: {self.descriçao}')
 
-class menu:
+class Prato:
     def __init__(self):
-        self.adicionar_produto = []
+        self.nome = ''
+        self.valor = ''
+        self.descriçao = ''
 
-    def adicionar_produto(self, restaurante):
-        self.adicionar_produto.append(restaurante)
+    def adicionar_nome(self, nome):
+        self.nome = nome
+        
+    def adicionar_valor(self, valor):
+        self.valor = valor
 
-    def consultar_carro(self):
-        for restaurante in self.adicionar_produto:
-            restaurante.escrever_nome()
-            restaurante.escrever_valor()
-            restaurante.escrever_descriçao()
-            print('')
+    def adicionar_descriçao(self, descriçao):
+        self.descriçao = descriçao
+
+    def escrever_nome(self):
+        print(f'nome: {self.nome}')
+        
+    def escrever_valor(self):
+        print(f'valor: {self.valor}')
+
+    def escrever_descriçao(self):
+        print(f'descriçao: {self.descriçao}')
+
+class Drink:
+    def __init__(self):
+        self.nome = ''
+        self.valor = ''
+        self.descriçao = ''
+
+    def adicionar_nome(self, nome):
+        self.nome = nome
+        
+    def adicionar_valor(self, valor):
+        self.valor = valor
+
+    def adicionar_descriçao(self, descriçao):
+        self.descriçao = descriçao
+
+    def escrever_nome(self):
+        print(f'nome: {self.nome}')
+        
+    def escrever_valor(self):
+        print(f'valor: {self.valor}')
+
+    def escrever_descriçao(self):
+        print(f'descriçao: {self.descriçao}')
+
+class Sobremesa:
+    def __init__(self):
+        self.nome = ''
+        self.valor = ''
+        self.descriçao = ''
+
+    def adicionar_nome(self, nome):
+        self.nome = nome
+        
+    def adicionar_valor(self, valor):
+        self.valor = valor
+
+    def adicionar_descriçao(self, descriçao):
+        self.descriçao = descriçao
+
+    def escrever_nome(self):
+        print(f'nome: {self.nome}')
+        
+    def escrever_valor(self):
+        print(f'valor: {self.valor}')
+
+    def escrever_descriçao(self):
+        print(f'descriçao: {self.descriçao}')
--- Restaurante.py
+++ Restaurante.py
@@ -1,4 +1,4 @@
-from menu import restaurante, menu
+from Menu import Bebida, Prato, Sobremesa, Drink, menu
 
 print('--- Menu de Restaurante ---')
 
@@ -6,14 +6,17 @@ print('')
 print('--- Opções ---')
 print('')
 
-print('--- 1 - Cadastrar Produto ')
-print('--- 2 - Consultar Menu ')
-print('--- 3 - Sair ')
+print('--- 1 - Cadastrar Bebida ')
+print('--- 2 - Cadastrar Prato ')
+print('--- 3 - Cadastrar Sobremesa ')
+print('--- 4 - Cadastrar Drink ')
+print('--- 5 - Consultar Menu ')
+print('--- 6 - Sair ')
 
 value = 0
-lista_de_produtos = restaurante()
+_menu = menu()
 
-while( value != 3 ):
+while( value != 6 ):
 
     print('')
     value = int(input('Digite a sua opção: '))
@@ -21,21 +24,77 @@ while( value != 3 ):
 
     match value:
         case 1:
-            _restaurante = restaurante()
-            _restaurante.adicionar_nome(input('Digite o Nome:'))
-            _restaurante.adicionar_valor(input('Digite o Valor: '))
-            _restaurante.adicionar_descriçao(input('Digite a Descrição: '))
+            _bebida = Bebida()
+            _bebida.adicionar_nome(input('Digite o Nome:'))
+            _bebida.adicionar_valor(input('Digite o Valor: '))
+            _bebida.adicionar_descriçao(input('Digite a Descrição: '))
+
+            _menu.adicionar_bebida(_bebida)
         
             print('')
             print('Cadastrado com Sucesso')
             print('')
-
+        
         case 2:
-            print('Lista de produtos')
+            _prato = Prato()
+            _prato.adicionar_nome(input('Digite o Nome: '))
+            _prato.adicionar_valor(input('Digite o Valor: '))
+            _prato.adicionar_descriçao(input('Digite a Descrição: '))
+
+            _menu.adicionar_prato(_prato)
+
+            print('')
+            print('Cadastrado com Sucesso')
             print('')
 
         case 3:
+            _sobremesa = Sobremesa()
+            _sobremesa.adicionar_nome(input('Digite o Nome: '))
+            _sobremesa.adicionar_valor(input('Digite o Valor: '))
+            _sobremesa.adicionar_descriçao(input('Digite a Descrição: '))
+
+            _menu.adicionar_sobremesa(_sobremesa)
+
+            print('')
+            print('Cadastrado com Sucesso')
+            print('')
+
+        case 4:
+            _drink = Drink()
+            _drink.adicionar_nome(input('Digite o Nome: '))
+            _drink.adicionar_valor(input('Digite o valor: '))
+            _drink.adicionar_descriçao(input('Digite a Descrição: '))
+
+            _menu.adicionar_drink(_drink)
+
+            print('')
+            print('Cadastrado com Sucesso')
+            print('')
+
+
+        case 5:
+            print('Menu')
+            print('')
+
+            print('BEBIDAS')
+            print('')
+            _menu.consultar_bebidas()
+
+            print('DRINKS')
+            print('')
+            _menu.consultar_drinks()
+
+            print('PRATOS')
+            print('')
+            _menu.consultar_pratos()
+
+            print('SOBREMESA')
+            print('')
+            _menu.consultar_sobremesas()
+
+        case 6:
             print('Função de Sair')
+            
         case _:
             print('Função desconhecida')
 
